0a67af8ffde6155bd1283c1de014854fa904600b
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2024-04-09 08:12:19 +0200

    slides

--- src/app/pages/visit-page/visit-page.component.ts
+++ src/app/pages/visit-page/visit-page.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-visit-page',
   templateUrl: './visit-page.component.html',
-  styleUrls: ['./visit-page.component.css']
+  styleUrls: ['./visit-page.component.css'],
 })
 export class VisitPageComponent implements OnInit {
+  slide = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  triggerScan() {
+    console.log('triggerScan');
+    this.slide = 2;
   }
-
+  triggerTraining() {
+    console.log('triggerTraining');
+    this.slide = 3;
+  }
 }
